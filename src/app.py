94573c456ad65549a43607d7593f2b07df980b83
Rafael Goncalves <rgoncalves@vt.edu>
2024-09-06 09:55:33 +0200

    Add endpoint to get the stats for a given country

--- src/app.py
+++ src/app.py
@@ -1,6 +1,29 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from utils.utils import load_data, calculate_country_stats
 
 app = Flask(__name__)
+
+
+# Load dataset
+df = load_data("data/air-pollution.csv")
+
+
+@app.route("/api/v1/country", methods=["GET"])
+def get_country_pollution_stats():
+    try:
+        country = request.args.get("name").title()
+    except AttributeError:
+        country = None
+
+    if not country:
+        return jsonify({"error": "Please provide a country name"}), 400
+
+    country_stats = calculate_country_stats(df, country)
+
+    if country_stats is None:
+        return jsonify({"error": f"No data found for {country}"}), 404
+
+    return jsonify({"country": country, "statistics": country_stats})
 
 
 @app.route("/", methods=["GET"])
